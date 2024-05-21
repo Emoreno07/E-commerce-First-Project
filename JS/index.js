3507f3192fc215f3313830bcf9e4fec83639b7c7
Emoreno07 <162984812+Emoreno07@users.noreply.github.com>
2024-05-21 10:55:01 -0300

    added carrinho page and modified some properties

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,12 @@
-// document.getElementsByTagName('body')[0].onload = () =>{
-//     const arrayLivros = document.getElementsByClassName('livro');
-//     Array.prototype.forEach.call(arrayLivros, (element,index,array) => {
-//         element.onclick = () =>{
-//             for (const children  of this.children) {
-//                 if(children.tagName == "IMG"){object.img = children.src}
-//                 else if(children.tagName)
-//             }
-//             localStorage.setItem('currentlivro',)
-//         }
-//     })
-// }
+    // document.getElementsByTagName('body')[0].onload = () =>{
+    //     const arrayLivros = document.getElementsByClassName('livro');
+    //     Array.prototype.forEach.call(arrayLivros, (element,index,array) => {
+    //         element.onclick = () =>{
+    //             for (const children  of this.children) {
+    //                 if(children.tagName == "IMG"){object.img = children.src}
+    //                 else if(children.tagName)
+    //             }
+    //             localStorage.setItem('currentlivro',)
+    //         }
+    //     })
+    // }
