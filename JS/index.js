16d7d88c64f4bb2580920f712815fc2746fef46a
Emoreno <Emoreno22.0712@gmail.com>
2024-06-10 06:12:15 -0300

    added more js

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,5 @@
+//banco dos livros
+//paro casos de teste, as avaliações, rating e paginas estão usando Math.Random e não são fixas
 const ImagesNovidades = {
     images: [
     '../Images/A 5 passos de Voce.jpg',
@@ -5,11 +7,127 @@ const ImagesNovidades = {
     '../Images/A_socidade_do_anel.png',
     '../Images/a_torre_de_nero.png',
     '../Images/A-maldicao_do_tita.jpg'],
-
+    autores: [
+        'unknown',
+        'Rick Iordan',
+        'sei la',
+        'Rick Iordan',
+        'Rick Iordan',
+    ],
+    nome : [
+        'A 5 passos de você',
+        'A marca de atena',
+        'A sociedade do anel',
+        'A torre de nero',
+        'A maldição do tita'
+    ],
+    Paginas: [
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+    ]
 }
 
-const ImagesMaisVendidos = {}
- function CreateBook(img, rating, name, author)
+const ImagesMaisVendidos = {
+    images :[
+        '../Images/as_provacoes_de-apollo.png',
+        '../Images/batalha_do_labirinto.png',
+        '../Images/DUNA.jpg',
+        '../Images/É assim que acaba.png',
+        '../Images/harry potter e a pedra.png'
+
+    ],
+    autores :[
+        'Rick Iordan',
+        'Rick Iordan',
+        'Alguém',
+        'Alguém também',
+        'J.K Rowling'
+    ],
+    nome: [
+        'as provações de Apollo',
+        'A batalha do labirinto',
+        'DUNA',
+        'É assim que acaba',
+        'Harry potter e a pedra filosofal'
+    ],
+    Paginas: [
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+    ]
+}
+
+const ImagesInternacionais = {
+    images :[
+        '../Images/labirinto_de-fogo.png',
+        '../Images/mais_esperto_que_o_diabo.png',
+        '../Images/menino_pijama.png',
+        '../Images/O_filho_de_netuno.jfif',
+        '../Images/o_gambito_da_rainha.webp'
+
+    ],
+    autores :[
+        'Rick Iordan',
+        'Napoleon Hill',
+        'John Boyne',
+        'Rick Iordan',
+        'Walter'
+    ],
+    nome: [
+        'O Labirinto de fogo',
+        'Mais esperto que o diabo',
+        'o Menino do pijamas listrado',
+        'O filho de Netuno',
+        'O gambito da rainha'
+    ],
+    Paginas: [
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+    ]
+}
+const ImagesPodeSerDoSeuInteresse = {
+    images :[
+        '../Images/o_pequeno_principe.webp',
+        '../Images/O_ultimo_olimpiano.png',
+        '../Images/quem_mexeu_no_meu_queijo.png',
+        '../Images/reliquias da morte.png',
+        '../Images/the_house_of_hades.png'
+
+    ],
+    autores :[
+        'Antonie',
+        'Rick Iordan',
+        'Spencer Jhonson',
+        'J.K Rowling',
+        'Rick Iordan'
+    ],
+    nome: [
+        'O pequeno Príncipe',
+        'O ultimo Olimpiano',
+        'Quem mexeu no meu queijo',
+        'Harry Potter e as reliquias da morte',
+        'A casa de Hades'
+    ],
+    Paginas: [
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+        Math.trunc(Math.random() * 450),
+    ]
+}
+
+//funcção para criar e colocar um livro na categoria.
+
+ function CreateBook(img, rating, name, author,qtaPaginas)
 {
     const livro = document.createElement('div')
     livro.setAttribute('class','livro')
@@ -19,23 +137,40 @@ const ImagesMaisVendidos = {}
     livro.appendChild(imgLivro)
     const stars = document.createElement('div')
     stars.setAttribute('class','estrelas')
+
+    //colocando as estrelas de acordo com o rating(avaliações)
+    //coloca as primeiras x. estrelas do rating  (eg 4.6 rating = 4)
+
     for(let i = 0; i < Math.trunc(rating); i++){
         const star = document.createElement('img')
         star.src = '../Images/estrela-cheia.png'
         stars.appendChild(star)
     }
+    //adiciona uma estrela pela metade se o rating for decimal
+
     if(Math.trunc(rating) < rating)
         {
             let star_middle = document.createElement('img')
             star_middle.src = '../Images/estrela_metade.png'
             stars.appendChild(star_middle)
         }
+    //completa o resto das 5 estrelas com estrelas em branco, se houver
+
     for(let i = 0; i < Math.abs(Math.trunc(rating) - 4); i++)
         {
             let star_empty = document.createElement('img')
             star_empty.src = '../Images/star.png'
             stars.appendChild(star_empty)
         }
+        //informações adicionais
+    const aux = document.createElement('p')
+    aux.style.display = 'none'
+    aux.setAttribute('class','rating')
+    aux.innerHTML = Math.round(rating * 10) / 10
+    const paginas = document.createElement('p')
+    paginas.style.display = 'none'
+    paginas.setAttribute('class',"Paginas")
+    paginas.innerHTML = qtaPaginas
     livro.appendChild(stars)
     const autor = document.createElement('p')
     const nome = document.createElement('p')
@@ -43,9 +178,13 @@ const ImagesMaisVendidos = {}
     autor.setAttribute('class','autor')
     nome.innerHTML = name
     nome.setAttribute('class','titulo')
+
+    //aux é apenas referência do rating para estar no onClickListener do livro
+
     livro.appendChild(nome)
     livro.appendChild(autor)
-    
+    livro.appendChild(aux)
+    livro.appendChild(paginas)
     return livro
 }
 
@@ -53,18 +192,62 @@ const ImagesMaisVendidos = {}
 
 
     document.getElementsByTagName('body')[0].onload = () =>{
+        //itens no carringo(quantidade)
+
+        document.querySelector('#p_carrinho').innerHTML = 'Itens no carrinho: ' +
+        ((localStorage.getItem('carrinho') === null) ? 0 : JSON.parse(localStorage.getItem('carrinho')).items.length)
+
         document.querySelector('#imglogo').addEventListener('click',()=>{
             location.href = '../pages/index.html'
         })
+        //colocando os produtos nas categorias de livros
+        
         ImagesNovidades.images.forEach((element, index) =>{
             document.querySelector("#Novidades").appendChild(
                 CreateBook(
                     element,
                     ((Math.random() + 1) * 2),
-                    'titulo '+(index+1),
-                    'autor '+(index+1)))
+                    ImagesNovidades.nome[index],
+                    ImagesNovidades.autores[index],
+                    ImagesNovidades.Paginas[index]))
         })
-        
+
+        ImagesMaisVendidos.images.forEach((element, index ) =>{
+            document.querySelector('#MaisVendidos').appendChild(
+                CreateBook(
+                    element,
+                    ((Math.random() + 1) * 2),
+                    ImagesMaisVendidos.nome[index],
+                    ImagesMaisVendidos.autores[index],
+                    ImagesMaisVendidos.Paginas[index]
+                )
+            )
+            
+        })
+        ImagesInternacionais.images.forEach((element, index) =>{
+            document.querySelector('#LivrosInternacionais').appendChild(
+                CreateBook(
+                    element,
+                    ((Math.random() + 1) * 2),
+                    ImagesInternacionais.nome[index],
+                    ImagesInternacionais.autores[index],
+                    ImagesInternacionais.Paginas[index]
+                )
+            )
+        })
+        ImagesPodeSerDoSeuInteresse.images.forEach((element, index) =>{
+            document.querySelector('#Interesse').appendChild(
+                CreateBook(
+                    element,
+                    ((Math.random() + 1) * 2),
+                    ImagesPodeSerDoSeuInteresse.nome[index],
+                    ImagesPodeSerDoSeuInteresse.autores[index],
+                    ImagesPodeSerDoSeuInteresse.Paginas[index]
+                )
+            )
+        })
+
+        //colocando o click de enviar o livro para a página de produto
        const livros = document.querySelectorAll('.livro')
        livros.forEach((element) =>{
         element.addEventListener('click', function(){
@@ -72,8 +255,12 @@ const ImagesMaisVendidos = {}
                 'livroimg' : '',
                 'estrelasimgs' : [],
                 'titulo' : '',
-                "autor" : ''
+                "autor" : '',
+                'rating' : 0,
+                'paginas': 0
             }
+
+            //não consegui desenvolver uma melhor lógica que esta
             for (const children of this.children) {
                 
                 if(children.className === 'imglivro')
@@ -94,9 +281,18 @@ const ImagesMaisVendidos = {}
                     {
                         json.autor = children.innerHTML
                     }
+                else if(children.className === 'rating')
+                    {
+                        json.rating = children.innerHTML
+                    }
+                else if(children.className === 'Paginas')
+                    {
+                        json.paginas = children.innerHTML
+                    }
             }
+            //guarda as informações e deixa na página de produto
             localStorage.setItem('CurrentProduct',JSON.stringify(json))
-            // location.href = '../pages/product.html'
+            location.href = '../pages/product.html'
         })
        })
     }
---
+++ JS/product.js
@@ -0,0 +1,55 @@
+
+
+
+
+document.getElementsByTagName('body')[0].onload = () =>{
+    //quantidade de itens no carrinho
+    document.querySelector('#p_carrinho').innerHTML = 'Itens no carrinho: ' +
+    ((localStorage.getItem('carrinho') === null) ? 0 : JSON.parse(localStorage.getItem('carrinho')).items.length)
+    document.querySelector('#imglogo').addEventListener('click',()=>{
+            location.href = '../pages/index.html'
+        })
+    //colocando as informações do livro
+    //Import estava dando problemas por tanto decidi optar por refazer(copiar e colar) codigo do CreateBook
+    const selected_product = JSON.parse(localStorage.getItem('CurrentProduct'))
+    const imgProduct = document.querySelector("#img_livro_produto")
+    imgProduct.src = selected_product.livroimg
+    const rating = selected_product.rating
+    const stars = document.querySelector('#estrelas')
+    const desc = document.querySelector('#titulo')
+    desc.innerHTML = selected_product.titulo
+    document.querySelector('#avaliações').innerHTML = 'Avaliações: ' + Math.round(Math.random()* 10)
+    document.querySelector("#autor").innerHTML = selected_product.autor
+    document.querySelector('#Paginas').innerHTML = selected_product.paginas + ' Páginas'
+    for(let i = 0; i < Math.trunc(rating); i++){
+        const star = document.createElement('img')
+        star.src = '../Images/estrela-cheia.png'
+        stars.appendChild(star)
+    }
+    if(Math.trunc(rating) < rating)
+        {
+            let star_middle = document.createElement('img')
+            star_middle.src = '../Images/estrela_metade.png'
+            stars.appendChild(star_middle)
+        }
+    for(let i = 0; i < Math.abs(Math.trunc(rating) - 4); i++)
+        {
+            let star_empty = document.createElement('img')
+            star_empty.src = '../Images/star.png'
+            stars.appendChild(star_empty)
+        }
+    var items = {
+        'items' : []
+    }
+    //adiciona no carrinho
+
+    document.querySelector('#add_carrinho').onclick = () =>{
+        if(localStorage.getItem('carrinho') === null)
+            localStorage.setItem('carrinho',JSON.stringify(items))
+        const carrinho = JSON.parse(localStorage.getItem('carrinho'))
+        carrinho.items.push(localStorage.getItem('CurrentProduct'))
+        localStorage.setItem('carrinho',JSON.stringify(carrinho))
+        alert("O seu produto foi adicionado ao carrinho")
+        location.reload()
+    }
+}
